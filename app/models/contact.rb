32dcbf411a33e9312b686dfbcf6b8596a6af648b
Scotchard22 <scotchard@aol.com>
2016-06-27 19:19:25 -0400

    changed phone number input

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < MailForm::Base
   attribute :name,          :validate => true
-  attribute :phone_number,  :validate => /\A\d{3}-\d{3}-\d{4}\z/ 
+  attribute :phone_number,  :validate => /\A\d{3}\d{3}\d{4}\z/ 
   attribute :email,         :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message,       :validate => true
   attribute :nickname,      :captcha => true
